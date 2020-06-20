47aeb8ae8c826c82aaff775056107de414727f1b
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-06-20 11:52:53 -0500

    renamed variables to match other functions

--- R/gage_labels.R
+++ R/gage_labels.R
@@ -1,17 +1,17 @@
 #' @tile Create Gage Labels
 #'
-#' @description Create a data frame of gage labels to be used in a longitudinal
+#' @description Create a data frame of gage labels used by the longitudinal
 #' profile plot.
 #'
 #' @param gage_locations       data frame; A data frame of gage locations.
-#' @param stage_interval       numeric; The number of feet between gage stage
+#' @param stage_interval_label numeric; The number of feet between gage stage
 #'                             labels. Used to identify the location of gage
 #'                             labels.
 #'
 #' @return A data frame of gage labels used by the `razviz` longitudinal profile
 #' plot.
 #'
-gage_labels <- function(gage_locations, stage_interval) {
+gage_labels <- function(gage_locations, stage_interval_label) {
   # Create the data frame to hold gage stage labels.
   gage_labels <- data.frame(name = character(),
                             river_mile = double(),
@@ -25,11 +25,11 @@ gage_labels <- function(gage_locations, stage_interval) {
     river_mile <- gage$river_mile
 
     # Identify min and max stage label, rounded to the nearest stage_gage_label.
-    min_stage  <- ceiling(gage$min_stage / stage_interval) * stage_interval
-    max_stage  <- floor(gage$max_stage / stage_interval) * stage_interval
+    min_stage <- ceiling(gage$min_stage / stage_interval_label) * stage_interval_label
+    max_stage <- floor(gage$max_stage / stage_interval_label) * stage_interval_label
 
     # Iterate through the min and max stages for each gage creating the labels.
-    for (j in seq(min_stage, max_stage, by = stage_interval)) {
+    for (j in seq(min_stage, max_stage, by = stage_interval_label)) {
       elevation   <- gage$elevation + j
       stage       <- j
       gage_labels <- rbind(gage_labels,
