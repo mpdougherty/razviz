424bf880a83b1c121a1745b45fb4dc6d53861ccb
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-06-23 08:22:11 -0500

    added the high_water_events function

---
+++ R/high_water_events.R
@@ -0,0 +1,29 @@
+#' @title High Water Events
+#'
+#' @description Filter a data frame of high water marks for a specific list of
+#' high water marks.
+#'
+#' @export
+#' @param high_water     data frame; A data frame of high water marks. See the
+#'                       package dataset for the required fields.
+#' @param events         character vector; A character vector of high water
+#'                       event years (i.e., c("1993", "2008")).
+#'
+#' @return A data frame of high_water_events filtered by the `event` years.
+#'
+#' @importFrom lubridate mdy year
+#' @importFrom dplyr filter
+#'
+
+high_water_events <- function(high_water, events) {
+  # Ensure that the peak_date field is formated correctly
+  high_water$peak_date <- lubridate::mdy(high_water$peak_date)
+
+  # Create `event` field as year of `peak_date` field
+  high_water$event <- as.character(lubridate::year(high_water$peak_date))
+
+  # Filter for the desired list of high water event years
+  high_water <- dplyr::filter(high_water, event %in% events)
+
+  return(high_water)
+}
---
+++ tests/testthat/test_high_water_events.R
@@ -0,0 +1,14 @@
+context("high water event")
+library(razviz)
+
+# high_water
+high_water_csv <- system.file("extdata", "high_water_marks.csv",
+                              package = "razviz")
+high_water <- readr::read_csv(high_water_csv)
+high_water_years <- c("2008", "2013", "2014")
+high_water_events_df <- razviz::high_water_events(high_water, high_water_years)
+
+
+test_that("high water event", {
+  expect_true(is.data.frame(high_water_events_df))
+})
