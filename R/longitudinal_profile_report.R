d0bb5b44bcf10be959190e9ae8fcc3b32a37062b
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-06-23 14:07:18 -0500

    added the longitudinal_profile_report function

---
+++ R/longitudinal_profile_report.R
@@ -0,0 +1,69 @@
+#' @title Longitudinal Profile Report
+#'
+#' @description Create a Longitudinal Profile Report for the input RAS model.
+#'
+#' @export
+#' @param hydro_model     data frame; A data frame of results from a RAS model.
+#'                        See the package dataset for required fields.
+#' @param long_plot_pgs   data frame; A data frame of longitudinal plot pages
+#'                        produced by the `razviz::long_plot_pages` function.
+#' @param gages           data frame. A data frame of gages. See package dataset
+#'                        for required fields.
+#' @param gage_labels_df  data frame; A data frame of gage labels produced by
+#'                        the `razviz::gage_labels` function.
+#' @param gage_boxes_df   data frame; A data frame of gage boxes produced by the
+#'                        `razviz::gage_boxes` function.
+#' @param high_water      data frame; A data frame of high water marks. See the
+#'                        package dataset for required fields.
+#' @param levees          data frame; A data frame of levee elevations. See the
+#'                        package dataset for required fields.
+#' @param features        data frame; A data frame of salient river features
+#'                        (e.g., tributary confluences, cities, bridges, dams,
+#'                        etc.). See package dataset for required fields.
+#' @param bridges         data frame; A data frame of bridge opening elevations.
+#'                        See package dataset for required fields.
+#' @param graph_colors    character vector; A named character vector of colors
+#'                        to use for each series.
+#' @param legend_labels   character vector; A character vector of labels to be
+#'                        used in the legend.
+#' @param plot_labels     list; A list of plot labeling elements. Must contain
+#'                        three named elements "title", "x_axis", and "y_axis".
+#' @param output_dir      character; The path to the folder where the output
+#'                        report will be written.
+#'
+#' @return A Longitudinal Profile Report in `pdf` document format.
+#'
+longitudinal_profile_report <- function(hydro_model, long_plot_pgs,
+                                        gages, gage_labels_df, gage_boxes_df,
+                                        high_water, levees,
+                                        features, bridges,
+                                        graph_colors, legend_labels, plot_labels,
+                                        output_dir) {
+  # Set the output document
+  output_file <- file.path(output_dir, "Longitudinal_Profile_Report.pdf")
+  pdf(file = output_file, width = 16.5, height = 10.5)
+
+  # Iterate through plot pages to produce longitudinal profile plots
+  for (i in long_plot_pgs$plot) {
+    # insert vertical white space so that the next figure is on a new page
+    cat('\\newpage')
+    # Set the current plot number
+    plot_number <- long_plot_pgs[long_plot_pgs$plot == i,]$plot
+    # Create the longitudinal profile plot
+    print(longitudinal_profile_plot(plot_number = plot_number,
+                                    hydro_model = hydro_model,
+                                    long_plot_pgs = long_plot_pgs,
+                                    gages = gages,
+                                    gage_labels_df = gage_labels_df,
+                                    gage_boxes_df = gage_boxes_df,
+                                    high_water = high_water,
+                                    levees = levees,
+                                    features = features,
+                                    bridges = bridges,
+                                    graph_colors = cols,
+                                    legend_labels = legend_labels,
+                                    plot_labels = plot_labels))
+  }
+  # Close the file and the graphics device
+  dev.off()
+}
---
+++ tests/testthat/test_longitudinal_profile_report.R
@@ -0,0 +1,104 @@
+context("longitudinal profile report")
+library(razviz)
+
+# hydro_model
+path <- system.file("extdata", package = "razviz")
+pattern <- "Freq"
+hydro_model <- razviz::combine_files(path = path, pattern = pattern)
+## Create `Event` field for labeling
+hydro_model$Event <- hydro_model$Freq
+## Filter the Events
+model_events <- c("2 Year", "100 Year", "500 Year", "100000 Year")
+hydro_model_1 <- dplyr::filter(hydro_model, Event %in% model_events)
+## Set Event as an ordered factor
+hydro_model_1$Event <- factor(hydro_model_1$Event,
+                              levels = model_events,
+                              labels = model_events)
+
+# high_water
+high_water_csv <- system.file("extdata", "high_water_marks.csv",
+                              package = "razviz")
+high_water <- readr::read_csv(high_water_csv)
+high_water_years <- c("2008", "2013", "2014")
+high_water_events_df <- razviz::high_water_events(high_water, high_water_years)
+## Set Event as an ordered factor
+high_water_events_df$event <- factor(high_water_events_df$event,
+                                     levels = high_water_years,
+                                     labels = high_water_years)
+
+# long_plot_pages
+miles_per_plot <- 60
+long_plot_pages_df <- razviz::long_plot_pages(hydro_model,
+                                              high_water_events_df,
+                                              miles_per_plot)
+
+# gages
+gage_csv <- system.file("extdata", "gage_locations.csv", package = "razviz")
+gages <- readr::read_csv(gage_csv)
+
+## Remove double backslashes introduced by R import
+gages$name <- gsub("\\n", "\n", gages$name, fixed=TRUE)
+
+# gage_labels
+stage_interval_label <- 5
+gage_labels_df <- razviz::gage_labels(gages, stage_interval_label)
+
+# gage_boxes
+stage_interval_boxes <- 1
+box_width <- 0.1
+gage_boxes_df <- razviz::gage_boxes(gages, stage_interval_boxes, box_width)
+
+# levees
+levees_csv <- system.file("extdata", "levees_authorized_existing.csv",
+                          package = "razviz")
+levees <- readr::read_csv(levees_csv)
+
+# features
+features_csv <- system.file("extdata", "river_features.csv",
+                            package = "razviz")
+features <- readr::read_csv(features_csv)
+
+# bridges
+bridges_csv <- system.file("extdata", "bridge_elevations.csv",
+                           package = "razviz")
+bridges <- readr::read_csv(bridges_csv)
+
+# graph_colors https://wesandersonpalettes.tumblr.com, names from colors().
+cols <- c("2 Year"      = "darkslategray4",
+          "100 Year"    = "cadetblue3",
+          "500 Year"    = "coral3",
+          "100000 Year" = "burlywood3",
+          "2008"        = "red",
+          "2013"        = "red",
+          "2014"        = "red",
+          "LEFT"        = "palevioletred2",
+          "RIGHT"       = "palevioletred4")
+
+# legend_labels
+legend_labels <- c("2 Year", "100 Year", "500 Year", "100000 Year",
+                   "2008", "2013", "2014",
+                   "Left Bank", "Right Bank")
+
+# title
+plot_labels <- list("title" = "Upper Mississippi River Hydraulic Model - A to B",
+                    "x_axis" = "Miles Above the Ohio River",
+                    "y_axis" = "Elevation (NAVD88 feet)")
+
+# output_dir
+#temp_dir <- tempdir()
+temp_dir <- "D:/Workspace/ECHH_Tools"
+
+# longitudinal_profile_report
+longitudinal_profile_report(hydro_model = hydro_model_1,
+                            long_plot_pgs = long_plot_pages_df,
+                            gages = gages,
+                            gage_labels_df = gage_labels_df,
+                            gage_boxes_df = gage_boxes_df,
+                            high_water = high_water_events_df,
+                            levees = levees,
+                            features = features,
+                            bridges = bridges,
+                            graph_colors = cols,
+                            legend_labels = legend_labels,
+                            plot_labels = plot_labels,
+                            output_dir = temp_dir)
