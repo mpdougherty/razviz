95cb24e6f51d8a3725f8eaada9dff3067c67564a
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-06-28 10:17:08 -0500

    added lengthen_hydrograph function

---
+++ R/lengthen_hydrographs.R
@@ -0,0 +1,25 @@
+#' @title Lengthen Hydrographs
+#'
+#' @description Converts a data frame of RAS hydrograph export data from
+#' wide to long format required for plotting.
+#'
+#' @export
+#' @param hydrograph_df    data frame; A data frame of RAS hydrograph export
+#'                         data.
+#'
+#' @return A data frame of RAS hydrograph export data in long format.
+#'
+#' @importFrom tidyr pivot_longer
+#'
+lengthen_hydrographs <- function(hydrograph_df) {
+  long_hydrograph <- tidyr::pivot_longer(hydrograph_df,
+                                         cols = c(WS_Elev, Obs_WS, Model_Q,
+                                                  Obs_Q),
+                                         names_to = "Type",
+                                         values_to = "value")
+
+  # Assign factors
+  long_hydrograph$Type <- factor(long_hydrograph$Type)
+
+  return(long_hydrograph)
+}
---
+++ tests/testthat/test_lengthen_hydrographs.R
@@ -0,0 +1,62 @@
+context("lengthen hydrographs")
+library(razviz)
+
+# Event 2008, Calibration #9
+folder <- system.file("extdata/hydrographs/2008_calibration_9",
+                      package = "razviz")
+event <- "2008"
+run_number <- 9
+run_type <- "Calibration"
+cal_2008 <- razviz::import_ras_hydrographs(folder = folder,
+                                           event = event,
+                                           run_number = run_number,
+                                           run_type = run_type)
+
+# Event 2013, Calibration #9
+folder <- system.file("extdata/hydrographs/2013_calibration_9",
+                      package = "razviz")
+event <- "2013"
+run_number <- 9
+run_type <- "Calibration"
+cal_2013 <- razviz::import_ras_hydrographs(folder = folder,
+                                           event = event,
+                                           run_number = run_number,
+                                           run_type = run_type)
+
+# Event 2014, Calibration #9
+folder <- system.file("extdata/hydrographs/2014_calibration_9",
+                      package = "razviz")
+event <- "2014"
+run_number <- 9
+run_type <- "Calibration"
+cal_2014 <- razviz::import_ras_hydrographs(folder = folder,
+                                           event = event,
+                                           run_number = run_number,
+                                           run_type = run_type)
+
+# Event 2017, Calibration #9
+folder <- system.file("extdata/hydrographs/2017_calibration_9",
+                      package = "razviz")
+event <- "2017"
+run_number <- 9
+run_type <- "Calibration"
+cal_2017 <- razviz::import_ras_hydrographs(folder = folder,
+                                           event = event,
+                                           run_number = run_number,
+                                           run_type = run_type)
+
+# Combine hydrograph events
+hydrograph_list <- list(cal_2008, cal_2013, cal_2014, cal_2017)
+cal_wide <- razviz::combine_hydrographs(hydrograph_list)
+
+# Convert to long format suitable for plotting
+cal <- razviz::lengthen_hydrographs(cal_wide)
+
+hydrograph_variables <- c("Model_Q", "Obs_Q", "Obs_WS", "WS_Elev")
+
+
+test_that("lengthen hydrographs", {
+  expect_true(is.data.frame(cal))
+  expect_true(all(levels(cal$Type) == hydrograph_variables))
+})
+
