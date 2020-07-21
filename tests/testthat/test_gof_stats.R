4d32cf7654207dec5c3137459177e0199326d063
Michael Dougherty <mike.p.doc.71@gmail.com>
2020-07-21 17:28:01 -0500

    added gof_stats test function

--- tests/testthat/test_gof_stats.R
+++ tests/testthat/test_gof_stats.R
@@ -9,6 +9,19 @@ hydrograph <- readr::read_csv(hydrograph_csv)
 # Calculate goodness of fit statistics
 hydrograph_stats <- gof_stats(hydrograph)
 
+# Import hydrograph data
+hydrograph_csv <- system.file("extdata/hydrographs", "hydrograph_data.csv",
+                              package = "razviz")
+hydrograph2 <- readr::read_csv(hydrograph_csv)
+
+# Filter for Mississippi River
+mr <- dplyr::filter(hydrograph2, River == "Mississippi")
+
+# Calculate goodness of fit statistics
+hydrograph_stats2 <- gof_stats(mr)
+
+hydrograph <- mr
+
 
 test_that("gof_stats", {
   expect_true(is.data.frame(hydrograph_stats))
