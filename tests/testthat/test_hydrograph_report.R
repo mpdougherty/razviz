178290980ad89a3b08fb5392d38a4bb79acadf34
Leigh Allison <leigh.a.allison@usace.army.mil>
2020-09-21 18:41:00 -0500

    Updated test to show new functions which automatically bring in data from dss

--- tests/testthat/test_hydrograph_report.R
+++ tests/testthat/test_hydrograph_report.R
@@ -1,79 +1,120 @@
 context("hydrograph report")
 library(razviz)
+library(readr)
+library(readtext)
+library(stringr)
 
-# Event 2008, Calibration #9
-folder <- system.file("extdata/hydrographs/2008_calibration_9",
-                      package = "razviz")
-event <- "2008"
-run_number <- 9
+software_file <- system.file("development/HEC-DSSVue-v3.0.00.212/HEC-DSSVue-v3.0.00.212", package = "razviz")
+options(dss_location=software_file)
+
+library(rJava)
+library(dssrip)
+
+#to remove scientific notation
+options(scipen=999)
+
+path <- system.file("extdata/hydrographs", package = "razviz")
+dss_observed_filename <- "razviz_ex_observed.dss"
+
+observed_dss_file <- dssrip::opendss(paste0(path,"/", dss_observed_filename), stopIfNew = TRUE)
+
+dss_model_filename <- "razviz_ex_model.dss"
+model_dss_file <- dssrip::opendss(paste0(path,"/", dss_model_filename), stopIfNew = TRUE)
+
+time_interval <- "6HOUR"
+plan_names <- c("UMR_PHASEIV_2001MOD-EVENT","UMR_PHASEIV_2014-EVENT","UMR_PHASEIV_2019MOD-EVENT")
+plan_events <- c(2001, 2014, 2019)
+
+
+filelist = list.files(path, pattern="*.txt")
+
+textfiles <- readtext::readtext(paste0(path,"/*.txt"), docvarsfrom = "filepaths")
+
+for(i in 1:length(plan_events)){
+  assign(paste0("UnsteadyFlowFile_",plan_events[i]),textfiles$text[i])
+}
+
+unsteadyflowfile_list <- mget(ls(pattern="UnsteadyFlowFile_"))
+
+Formatted_UnsteadyFlowFileList <- razviz::import_hecras_unsteadyflowfiles(
+  unsteadyflowfile_list = unsteadyflowfile_list,
+  plan_events = plan_events)
+
+list_observed_data <- razviz::import_observed_data(observed_dss_file = observed_dss_file,
+  plan_events = plan_events,
+  Formatted_UnsteadyFlowFileList = Formatted_UnsteadyFlowFileList)
+
+
+
+list_1Dmodel_data <- razviz::import_hecras_1Dmodel_data(model_dss_file = model_dss_file,
+  plan_names = plan_names,
+  plan_events = plan_events,
+  time_interval = time_interval,
+  Formatted_UnsteadyFlowFileList = Formatted_UnsteadyFlowFileList)
+
+
+
+SA2D_CONNECTIONS <- read.csv(paste0(path,"/UMR_2DFLOWAREAS.csv"))
+
+#Clean up data frame so that only 2D areas that have cross sections associated are kept.
+SA2D_CONNECTIONS <- SA2D_CONNECTIONS[!(is.na(SA2D_CONNECTIONS$CX_RM) | SA2D_CONNECTIONS$CX_RM==""), ]
+
+list_2Dmodel_data <- razviz::import_hecras_2Dmodel_data(model_dss_file = model_dss_file,
+  plan_names = plan_names,
+  plan_events = plan_events,
+  time_interval = time_interval,
+  Formatted_UnsteadyFlowFileList = Formatted_UnsteadyFlowFileList,
+  SA2D_CONNECTIONS = SA2D_CONNECTIONS)
+
+
+#Identify if modeled data is a calibration run or final model
 run_type <- "Calibration"
-cal_2008 <- razviz::import_ras_hydrographs(folder = folder,
-                                           event = event,
-                                           run_number = run_number,
-                                           run_type = run_type)
+#Identify which calibration run it is
+run_number <- 1
 
-# Event 2013, Calibration #9
-folder <- system.file("extdata/hydrographs/2013_calibration_9",
-                      package = "razviz")
-event <- "2013"
-run_number <- 9
-run_type <- "Calibration"
-cal_2013 <- razviz::import_ras_hydrographs(folder = folder,
-                                           event = event,
-                                           run_number = run_number,
-                                           run_type = run_type)
+#Make a list of river miles where there are gage locations in the RAS model
+Formatted_UnsteadyFlowFile <- Formatted_UnsteadyFlowFileList[[1]]
+locations <- unique(Formatted_UnsteadyFlowFile$RiverMile)
 
-# Event 2014, Calibration #9
-folder <- system.file("extdata/hydrographs/2014_calibration_9",
-                      package = "razviz")
-event <- "2014"
-run_number <- 9
-run_type <- "Calibration"
-cal_2014 <- razviz::import_ras_hydrographs(folder = folder,
-                                           event = event,
-                                           run_number = run_number,
-                                           run_type = run_type)
+hydrograph_datatables <- razviz::hydrograph_datatables(list_observed_data = list_observed_data,
+                                                       list_1Dmodel_data = list_1Dmodel_data,
+                                                       list_2Dmodel_data = list_2Dmodel_data,
+                                                       locations = locations,
+                                                       plan_events = plan_events,
+                                                       run_type = run_type,
+                                                        run_number = run_number)
 
-# Event 2017, Calibration #9
-folder <- system.file("extdata/hydrographs/2017_calibration_9",
-                      package = "razviz")
-event <- "2017"
-run_number <- 9
-run_type <- "Calibration"
-cal_2017 <- razviz::import_ras_hydrographs(folder = folder,
-                                           event = event,
-                                           run_number = run_number,
-                                           run_type = run_type)
-
-# Combine hydrograph events
-hydrograph_list <- list(cal_2008, cal_2013, cal_2014, cal_2017)
-cal_wide <- razviz::combine_hydrographs(hydrograph_list)
+#combine and reformat datatable
+hydrograph_df_wide <- razviz::combine_hydrographs(hydrograph_datatables) #wide version
 
 # Convert to long format suitable for plotting
-hydrograph_df <- razviz::lengthen_hydrographs(cal_wide)
+hydrograph_df <- razviz::lengthen_hydrographs(hydrograph_df_wide) #long version
 
-# Rename factors for prettier plot labeling
-hydrograph_df$River <- forcats::fct_recode(hydrograph_df$River,
-                                           "Mississippi" = "MISSISSIPPI")
-hydrograph_df$Reach <- forcats::fct_recode(hydrograph_df$Reach,
-                                           "Big Muddy to Ohio"      = "BIG MUDDY_OHIO",
-                                           "Fox to Bear"            = "FOXTOBEAR",
-                                           "Illinois to Mizzou"     = "ILLINOIS_MIZZOU",
-                                           "Iowa to Des Moines"     = "IOWATODESM",
-                                           "Kaskaskia to Big Muddy" = "KASKY_BIGMUDDY",
-                                           "Meramec to Kaskaskia"   = "MERAMEC_KASKY",
-                                           "Missouri to Meramec"    = "MISSOURI_MERAMEC",
-                                           "North to Salt"          = "NORTHTOSALT",
-                                           "Salt to Cuivre"         = "SALT_CUIVRE",
-                                           "Wyaconda to Fabius"     = "WYACONDATOFABIUS")
+#remove missing values
+hydrograph_df_na <- hydrograph_df[!is.na(hydrograph_df$value),]
 
-# Create hydrograph plot pages
-hg_plot_pages <- razviz::hydrograph_plot_pages(hydrograph_df)
+#rename the levels to what will be shown on plots.
+hydrograph_df_na$Event <- factor(hydrograph_df_na$Event, levels = c("2001","2014","2019"),
+  labels = c("2001MOD","2014","2019MOD") )
+
+#assign the group of data a run number and type
+hydrograph_df_na$Run_num <- 1
+hydrograph_df_na$Run_type <- "Calibration"
+
 
 # Create a hydrograph report
 output_dir <- tempdir()
 
-razviz::hydrograph_report(hydrograph_df, hg_plot_pages, output_dir)
+filename <- "Hydrograph_Plot_Report.pdf"
+
+#Define hydrograph plot pages
+hg_plot_pages <- razviz::hydrograph_plot_pages(hydrograph_df_na)
+
+
+# Create hydrograph report
+razviz::hydrograph_report(hydrograph_df = hydrograph_df_na,
+                          hg_plot_pages = hg_plot_pages,
+                          output_dir = output_dir, filename = filename)
 
 
 test_that("hydrograph report", {
