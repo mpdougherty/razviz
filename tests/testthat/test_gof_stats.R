e73719ed5347a1ecea3246bd20e7884b57d087e6
Leigh Allison <leigh.a.allison@usace.army.mil>
2020-10-02 16:44:37 -0500

    Added quotations around gof_stats in context.
    If not in quotes get this error:
    Error in cat(..., sep = "", file = self$out, append = TRUE) : argument 12 (type 'closure') cannot be handled by 'cat' Calls: <Anonymous> ... FUN -> <Anonymous> -> <Anonymous> -> <Anonymous> -> cat Execution halted

--- tests/testthat/test_gof_stats.R
+++ tests/testthat/test_gof_stats.R
@@ -1,26 +1,29 @@
-context(gof_stats)
+context("gof_stats")
 library(razviz)
 
 # Import hydrograph data
 hydrograph_csv <- system.file("extdata/hydrographs", "LD10.csv",
                               package = "razviz")
+
 hydrograph <- readr::read_csv(hydrograph_csv)
 
 # Calculate goodness of fit statistics
-hydrograph_stats <- gof_stats(hydrograph)
+
+
+hydrograph_stats <- razviz::gof_stats(hydrograph)
 
 # Import hydrograph data
-hydrograph_csv <- system.file("extdata/hydrographs", "hydrograph_data.csv",
-                              package = "razviz")
-hydrograph2 <- readr::read_csv(hydrograph_csv)
+#hydrograph_csv <- system.file("extdata/hydrographs", "hydrograph_data.csv",
+         #                     package = "razviz")
+#hydrograph2 <- readr::read_csv(hydrograph_csv)
 
 # Filter for Mississippi River
-mr <- dplyr::filter(hydrograph2, River == "Mississippi")
+#mr <- dplyr::filter(hydrograph2, River == "Mississippi")
 
 # Calculate goodness of fit statistics
-hydrograph_stats2 <- gof_stats(mr)
+#hydrograph_stats2 <- razviz::gof_stats(mr)
 
-hydrograph <- mr
+#hydrograph <- mr
 
 
 test_that("gof_stats", {
